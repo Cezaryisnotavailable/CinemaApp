ddb1eac744072a183485151c45d7d9ab26e7cac3
Cezary Cezary <cezary.game@gmail.com>
2023-03-09 19:35:07 +0100

    Screenings Views added

--- showtimes/views.py
+++ showtimes/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 
-from showtimes.models import Cinema
-from showtimes.serializers import CinemaSerializer
+from showtimes.models import Cinema, Screening
+from showtimes.serializers import CinemaSerializer, ScreeningSerializer
+
 
 # Create your views here.
 
@@ -14,5 +15,15 @@ class CinemaListView(generics.ListCreateAPIView):
 class CinemaView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cinema.objects.all()
     serializer_class = CinemaSerializer
+
+
+class ScreeningListView(generics.ListCreateAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
+
+
+class ScreeningView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
 
 
