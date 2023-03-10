8a8af0701e13759d2adf6cfccfe6f2007f4d5018
Cezary Cezary <cezary.game@gmail.com>
2023-03-10 21:36:43 +0100

    urls updated

--- CinemaApi/urls.py
+++ CinemaApi/urls.py
@@ -17,13 +17,15 @@ from django.contrib import admin
 from django.urls import path
 
 from project_cinema.views import MovieListView, MovieView
-from showtimes.views import CinemaListView, CinemaView
+from showtimes.views import CinemaListView, CinemaView, ScreeningView, ScreeningListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("movies/", MovieListView.as_view()),
-    path("movies/<int:pk>", MovieView.as_view(), name="movies-detail"),
+    path("movies/<int:pk>/", MovieView.as_view(), name="movies-detail"),
     path("cinemas/", CinemaListView.as_view()),
-    path("cinemas/<int:pk>", CinemaView.as_view()),
+    path("cinemas/<int:pk>/", CinemaView.as_view()),
+    path("screenings/", ScreeningListView.as_view()),
+    path("screenings/<int:pk>/", ScreeningView.as_view()),
 
 ]
